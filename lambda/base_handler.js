eec4b2dc85d5c5d70974bc72b583a9fa28510ca8
Alex Ough <alex.ough@sungardas.com>
2017-01-20 12:32:11 -0600

    added 'authorizer' in the return body

--- lambda/base_handler.js
+++ lambda/base_handler.js
@@ -10,6 +10,8 @@ exports.handler = (event, context) => {
   var path = paths[paths.length-1];
   var queryParams = event.queryStringParameters;
   var postData = (event.body) ? JSON.parse(event.body) : null;
+  var authorizer = event.requestContext.authorizer;
+  // { "refresh_token": "ffffffff-ffff-ffff-ffff-ffffffffffff", "principalId": "user|a1b2c3d4" }
 
   var credentials = null;
   if (event.headers.Credentials) {
@@ -24,44 +26,45 @@ exports.handler = (event, context) => {
     this[method](params, function(err, data) {
       if (err) {
         console.log(err);
-        sendFailureResponse({error: 'not permitted'}, 403, context);
+        sendFailureResponse({error: 'not permitted'}, 403, context, authorizer);
       }
       else {
         console.log(data);
-        sendSuccessResponse(data, context);
+        sendSuccessResponse(data, context, authorizer);
       }
     });
   }
   catch(err) {
     console.log(err);
-    sendNotPermittedMethodResponse(event.path, event.httpMethod, context);
+    sendNotPermittedMethodResponse(event.path, event.httpMethod, context, authorizer);
   }
 }
 
-function sendNotPermittedMethodResponse(path, method, context) {
+function sendNotPermittedMethodResponse(path, method, context, authorizer) {
   var responseBody = {error: "not permitted method " + method + " in " + path};
   var statusCode = 404;
-  sendResponse(responseBody, statusCode, context);
+  sendResponse(responseBody, statusCode, context, authorizer);
 }
 
-function sendNotFoundResponse(path, method, context) {
+function sendNotFoundResponse(path, method, context, authorizer) {
   var responseBody = {error: "invalid path " + path};
   var statusCode = 404;
-  sendResponse(responseBody, statusCode, context);
+  sendResponse(responseBody, statusCode, context, authorizer);
 }
 
-function sendSuccessResponse(retValue, context) {
+function sendSuccessResponse(retValue, context, authorizer) {
   var responseBody = retValue;
   var statusCode = 200;
-  sendResponse(responseBody, statusCode, context);
+  sendResponse(responseBody, statusCode, context, authorizer);
 }
 
-function sendFailureResponse(err, statusCode, context) {
+function sendFailureResponse(err, statusCode, context, authorizer) {
   var responseBody = err;
-  sendResponse(responseBody, statusCode, context);
+  sendResponse(responseBody, statusCode, context, authorizer);
 }
 
-function sendResponse(responseBody, statusCode, context) {
+function sendResponse(responseBody, statusCode, context, authorizer) {
+  responseBody['__authorizer'] = authorizer
   var response = {
       statusCode: statusCode,
       headers: { "Access-Control-Allow-Origin": "*" },
