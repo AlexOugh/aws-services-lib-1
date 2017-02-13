73d20734b3abe500872a4622b0bb1a43531644c3
Alex Ough <alex.ough@sungardas.com>
2017-02-13 15:22:28 -0600

    updated with the changes in base_handler

--- lambda/controller_handler.js
+++ lambda/controller_handler.js
@@ -9,27 +9,29 @@ exports.handler = (event, context) => {
   var paths = event.path.split('/');
   var path = paths[paths.length-1];
   var queryParams = event.queryStringParameters;
-  var postData = (event.body) ? JSON.parse(event.body) : null;
-  var authorizer = event.requestContext.authorizer;
+  var postData = (event.body) ? event.body : null;
+  if (postData && typeof(postData) == "string") postData = JSON.parse(postData);
+  var authorizer = (event.requestContext) ? event.requestContext.authorizer: null;
   // { "refresh_token": "ffffffff-ffff-ffff-ffff-ffffffffffff", "principalId": "user|a1b2c3d4" }
+  var resType = (event.resType) ? event.resType: null;
 
   var credentials = null;
   if (event.headers.Credentials) {
-    credentials = JSON.parse(event.headers.Credentials)
+    credentials = JSON.parse(new Buffer(event.headers.Credentials, 'base64').toString())
   }
   console.log(credentials);
 
+  if (authorizer == null && credentials == null) {
+    console.log("either Credentials or Customer Authorization result must be given");
+    sendFailureResponse({error: 'not permitted'}, 403, context, authorizer, resType);
+  }
+
   try {
-    // now find the target controller
-    method = method.toLowerCase();
-    var params = null;
-    if (method == 'get') {
-      params = queryParams;
-    }
-    else {
-      params = postData;
-    }
-    var controller = require('./' + path + '_controller');
+    var params = postData;
+    if (method == 'get') params = queryParams;
+    params['Credentials'] = credentials;
+
+    var controller = this.allocate_controller(path);
     console.log("controller: " + controller);
 
     // now check if the method exists in the found controller
@@ -41,49 +43,53 @@ exports.handler = (event, context) => {
     // run the method
     controller[method](params).then(data => {
       console.log(data);
-      sendSuccessResponse(data, context, authorizer);
+      sendSuccessResponse(data, context, authorizer, resType);
     }).catch(err => {
       console.log(err);
-      sendFailureResponse({error: err}, 500, context, authorizer);
+      sendFailureResponse({error: 'not permitted'}, 403, context, authorizer, resType);
     });
-
   }
   catch(err) {
     console.log(err);
-    sendNotPermittedMethodResponse(event.path, event.httpMethod, context, authorizer);
+    sendNotPermittedMethodResponse(event.path, event.httpMethod, context, authorizer, resType);
   }
 }
 
-function sendNotPermittedMethodResponse(path, method, context, authorizer) {
+function sendNotPermittedMethodResponse(path, method, context, authorizer, resType) {
   var responseBody = {error: "not permitted method " + method + " in " + path};
   var statusCode = 404;
-  sendResponse(responseBody, statusCode, context, authorizer);
+  sendResponse(responseBody, statusCode, context, authorizer, resType);
 }
 
-function sendNotFoundResponse(path, method, context, authorizer) {
+function sendNotFoundResponse(path, method, context, authorizer, resType) {
   var responseBody = {error: "invalid path " + path};
   var statusCode = 404;
-  sendResponse(responseBody, statusCode, context, authorizer);
+  sendResponse(responseBody, statusCode, context, authorizer, resType);
 }
 
-function sendSuccessResponse(retValue, context, authorizer) {
+function sendSuccessResponse(retValue, context, authorizer, resType) {
   var responseBody = retValue;
   var statusCode = 200;
-  sendResponse(responseBody, statusCode, context, authorizer);
+  sendResponse(responseBody, statusCode, context, authorizer, resType);
 }
 
-function sendFailureResponse(err, statusCode, context, authorizer) {
+function sendFailureResponse(err, statusCode, context, authorizer, resType) {
   var responseBody = err;
-  sendResponse(responseBody, statusCode, context, authorizer);
+  sendResponse(responseBody, statusCode, context, authorizer, resType);
 }
 
-function sendResponse(responseBody, statusCode, context, authorizer) {
-  responseBody['__authorizer'] = authorizer
+function sendResponse(responseBody, statusCode, context, authorizer, resType) {
+  if (authorizer) responseBody['__authorizer'] = authorizer
   var response = {
       statusCode: statusCode,
-      headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(responseBody)
   };
+  if (resType && resType == 'json') {
+    response['body'] = responseBody;
+  }
+  else {
+    response['headers'] = { "Access-Control-Allow-Origin": "*" };
+    response['body'] = JSON.stringify(responseBody);
+  }
   console.log("response: " + JSON.stringify(response))
   context.succeed(response);
 }
