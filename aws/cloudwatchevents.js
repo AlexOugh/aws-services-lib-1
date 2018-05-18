9ca95a2406dff8de97c8980a897825746aa47241
PratikB <ubuntu@ip-10-95-80-90.ec2.internal>
2018-05-18 10:04:31 +0000

    MA-3216:Created lib file for CloudWatch Events.

--- aws/cloudwatchevents.js
+++ aws/cloudwatchevents.js
@@ -21,7 +21,7 @@ function AWSCloudWatchEvents() {
 
   me.listRules = function(input, callback) {
 
-    var params = input.NamePrefix;
+    var params = {}
     console.log(params);
 
     var self = arguments.callee;
@@ -43,23 +43,79 @@ function AWSCloudWatchEvents() {
       }
     }
 
-    self.addParams = function(found) {
-      self.params.NamePrefix = found;
-    }
-
     var cloudwatchevents = me.preRun(self, input);
     cloudwatchevents.listRules(params, me.callbackFind);
   }
  
+  me.listTargetsByRule = function(input, callback) {
+
+    var params = {
+	Rule: input.ruleName
+    }
+    console.log(params);
+
+    var self = arguments.callee;
+
+    if (callback) {
+      var cloudwatchevents = me.findService(input);
+      cloudwatchevents.listTargetsByRule(params, callback);
+      return;
+    }
+
+    self.callbackFind = function(data) {
+      if (data[0]) {
+        console.log("found targets for rule");
+        return outputs[0];
+      }
+      else {
+        console.log("No targets found.");
+        return null;
+      }
+    }
+
+    var cloudwatchevents = me.preRun(self, input);
+    cloudwatchevents.listTargetsByRule(params, me.callbackFind);
+  }
+
+  me.listRuleNamesByTarget = function(input, callback) {
+
+    var params = {
+	TargetArn: input.targetArn
+    }
+    console.log(params);
+
+    var self = arguments.callee;
+
+    if (callback) {
+      var cloudwatchevents = me.findService(input);
+      cloudwatchevents.listRuleNamesByTarget(params, callback);
+      return;
+    }
+
+    self.callbackFind = function(data) {
+      if (data[0]) {
+        console.log("found rule for target");
+        return outputs[0];
+      }
+      else {
+        console.log("No rule found.");
+        return null;
+      }
+    }
+
+    var cloudwatchevents = me.preRun(self, input);
+    cloudwatchevents.listRuleNamesByTarget(params, me.callbackFind);
+  }
+
   me.createRule = function(input, callback) {
 
     var params = {
-      Name: input.name,
-      Description: input.description,
+      Name: input.ruleName,
+      Description: input.ruleDescription,
       EventPattern: input.eventPattern,
       RoleArn: input.roleArn,
    //   ScheduleExpression: 'STRING_VALUE',
-      State: input.state  //ENABLED | DISABLED
+      State: input.ruleState  //ENABLED | DISABLED
     };
     console.log(params);
 
@@ -81,7 +137,7 @@ function AWSCloudWatchEvents() {
   	Targets: [ /* required */
     	  {
       		Arn: input.topicArn, /* required */
-      		Id: input.id /* required */
+      		Id: input.targetId /* required */
       	/*	Input: input.inputstring,
       		InputPath: 'STRING_VALUE',
       		InputTransformer: {
@@ -120,8 +176,71 @@ function AWSCloudWatchEvents() {
     }
 
     var cloudwatchevents = me.preRun(self, input);
-    cloudwatchevents.deleteAlarms(params, me.callback);
+    cloudwatchevents.deleteRule(params, me.callback);
   }
+
+  me.removeTargets = function(input, callback) {
+
+    var params = {
+      Ids: [ input.targetId ],
+      Rule: input.ruleName
+    };
+    var self = arguments.callee;
+
+    if (callback) {
+      var cloudwatchevents = me.findService(input);
+      cloudwatchevents.removeTargets(params, callback);
+      return;
+    }
+
+    var cloudwatchevents = me.preRun(self, input);
+    cloudwatchevents.removeTargets(params, me.callback);
+  }
+  
+  me.describeRule = function(input, callback) {
+
+    var params = {
+      Name: input.ruleName
+    };
+    var self = arguments.callee;
+
+    if (callback) {
+      var cloudwatchevents = me.findService(input);
+      cloudwatchevents.describeRule(params, callback);
+      return;
+    }
+
+    var cloudwatchevents = me.preRun(self, input);
+    cloudwatchevents.describeRule(params, me.callback);
+  }
+
+  me.createEvent = function(input, callback) {
+
+    var params = {
+      Entries: [ input.eventEntries /* required */
+ /*      {
+        Detail: 'STRING_VALUE',
+        DetailType: 'STRING_VALUE',
+        Resources: [
+         'STRING_VALUE',
+        ],
+        Source: 'STRING_VALUE',
+        Time: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789
+       } */
+      ]
+    };
+    var self = arguments.callee;
+
+    if (callback) {
+      var cloudwatchevents = me.findService(input);
+      cloudwatchevents.putEvents(params, callback);
+      return;
+    }
+
+    var cloudwatchevents = me.preRun(self, input);
+    cloudwatchevents.putEvents(params, me.callback);
+  }
+
 }
 
 module.exports = AWSCloudWatchEvents
