1e87b64bfc2404ab0b67fe7fd9e45fa4e17ef39c
PratikB <ubuntu@ip-10-95-80-90.ec2.internal>
2018-09-04 13:29:13 +0000

    MA-4012: Updated kms.js and lambda.js lib files with additional functionalities .

--- aws_promise/kms.js
+++ aws_promise/kms.js
@@ -20,5 +20,37 @@ module.exports = {
       CiphertextBlob: new Buffer(input.password, 'base64')
     };
     return kms.decrypt(params).promise();
+  },
+
+  listKeyPolicies: function(input) {
+    var kms = this.findService(input);
+    var params = {
+	KeyId: input.keyId /* required */
+    };
+    return kms.listKeyPolicies(params).promise();
+  },
+
+  getkeyPolicy: function(input) {
+    var kms = this.findService(input);
+    var params = {
+	KeyId: input.keyId, /* required */ // The identifier of the CMK whose key policy you want to retrieve. You can use the key ID or the Amazon Resource Name (ARN) of the CMK.
+	PolicyName: input.policyName /* required */
+    };
+    return kms.getKeyPolicy(params).promise();
+  },
+
+  putkeyPolicy: function(input) {
+    var kms = this.findService(input);
+    var params = {
+	KeyId: input.keyId, /* required */
+	Policy: input.policy, /* required */
+	PolicyName: input.policyName /* required */
+    };
+  /* var params = {
+  KeyId: "1234abcd-12ab-34cd-56ef-1234567890ab", // The identifier of the CMK to attach the key policy to. You can use the key ID or the Amazon Resource Name (ARN) of the CMK.
+  Policy: "{\n    \"Version\": \"2012-10-17\",\n    \"Id\": \"custom-policy-2016-12-07\",\n    \"Statement\": [\n        {\n            \"Sid\": \"Enable IAM User Permissions\",\n            \"Effect\": \"Allow\",\n            \"Principal\": {\n                \"AWS\": \"arn:aws:iam::111122223333:root\"\n            },\n            \"Action\": \"kms:*\",\n            \"Resource\": \"*\"\n        },\n        {\n            \"Sid\": \"Allow access for Key Administrators\",\n            \"Effect\": \"Allow\",\n            \"Principal\": {\n                \"AWS\": [\n                    \"arn:aws:iam::111122223333:user/ExampleAdminUser\",\n                    \"arn:aws:iam::111122223333:role/ExampleAdminRole\"\n                ]\n            },\n            \"Action\": [\n                \"kms:Create*\",\n                \"kms:Describe*\",\n                \"kms:Enable*\",\n                \"kms:List*\",\n                \"kms:Put*\",\n                \"kms:Update*\",\n                \"kms:Revoke*\",\n                \"kms:Disable*\",\n                \"kms:Get*\",\n                \"kms:Delete*\",\n                \"kms:ScheduleKeyDeletion\",\n                \"kms:CancelKeyDeletion\"\n            ],\n            \"Resource\": \"*\"\n        },\n        {\n            \"Sid\": \"Allow use of the key\",\n            \"Effect\": \"Allow\",\n            \"Principal\": {\n                \"AWS\": \"arn:aws:iam::111122223333:role/ExamplePowerUserRole\"\n            },\n            \"Action\": [\n                \"kms:Encrypt\",\n                \"kms:Decrypt\",\n                \"kms:ReEncrypt*\",\n                \"kms:GenerateDataKey*\",\n                \"kms:DescribeKey\"\n            ],\n            \"Resource\": \"*\"\n        },\n        {\n            \"Sid\": \"Allow attachment of persistent resources\",\n            \"Effect\": \"Allow\",\n            \"Principal\": {\n                \"AWS\": \"arn:aws:iam::111122223333:role/ExamplePowerUserRole\"\n            },\n            \"Action\": [\n                \"kms:CreateGrant\",\n                \"kms:ListGrants\",\n                \"kms:RevokeGrant\"\n            ],\n            \"Resource\": \"*\",\n            \"Condition\": {\n                \"Bool\": {\n                    \"kms:GrantIsForAWSResource\": \"true\"\n                }\n            }\n        }\n    ]\n}\n", // The key policy document.
+  PolicyName: "default"// The name of the key policy.
+ }; */
+    return kms.getKeyPolicy(params).promise();
   }
 }
--- aws_promise/lambda.js
+++ aws_promise/lambda.js
@@ -53,5 +53,21 @@ module.exports = {
       params['SourceArn'] = input.sourceArn;
     }
     return lambda.addPermission(params).promise();
+  },
+
+  invokeFunction: function(input) {
+    var lambda = this.findService(input);
+    var invocationType= (input.invocationType) ? input.invocationType : "RequestResponse";
+    var logType = (input.logType) ? input.logType : "Tail";
+    var qualifier= (input.qualifier) ? input.qualifier : "$LATEST";
+    var params = {
+      FunctionName: input.functionArn,
+      ClientContext: input.clientContext, 
+      InvocationType: invocationType, 
+      LogType: logType, 
+      Payload: new Buffer(input.payload, 'base64'), 
+      Qualifier: qualifier 
+    };
+    return lambda.invoke(params).promise();
   }
 }
