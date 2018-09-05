b32cb8ecbf3b73b2d5691e790b171fe79ffc69bb
Mayank  Bharadwaj <mayank.bharadwaj@sungardas.com>
2018-09-05 16:08:20 +0530

    added new promise library for cfn

---
+++ aws_promise/awsCfnStack.js
@@ -0,0 +1,24 @@
+var AWS = require('aws-sdk');
+
+module.exports = {
+
+  findService: function(input) {
+    let  params = {region:input.region};
+    if (input.creds)  params.credentials = input.creds;
+    else if (input.profile) {
+      let credentials = new AWS.SharedIniFileCredentials({profile: input.profile});
+      AWS.config.credentials = credentials;
+    }
+    const cfn = new AWS.CloudFormation(params);
+    return cfn;
+  },
+
+  findStackOutputs: function(input) {
+    const cfn = this.findService(input);
+    let params = {
+        StackName: input.payload.stackName
+    };
+    return cfn.describeStacks(params).promise();
+}
+
+}
