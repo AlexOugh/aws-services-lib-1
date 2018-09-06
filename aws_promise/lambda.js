282c4eaa69c07a7434b777723c099d506b7c2678
PratikB <ubuntu@ip-10-95-80-90.ec2.internal>
2018-09-06 09:05:03 +0000

    MA-4012: Updated lambda.js lib file, converted payload JSON Object to string.

--- aws_promise/lambda.js
+++ aws_promise/lambda.js
@@ -66,7 +66,6 @@ module.exports = {
       ClientContext: input.clientContext, 
       InvocationType: invocationType, 
       LogType: logType, 
-//      Payload: new Buffer(payload, 'base64'), 
       Payload: payload, 
       Qualifier: qualifier 
     };
